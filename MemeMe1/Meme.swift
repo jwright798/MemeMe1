ab5c8cd7621492274125af21811e80766f424a84
Jeremy Wright <jwright798@gmail.com>
2017-06-29 21:25:32 -0500

    Creating v1 of the MemeMe app

---
+++ MemeMe1/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe1
+//
+//  Created by Jeremy Wright on 6/29/17.
+//  Copyright © 2017 Jeremy Wright. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memeImage : UIImage
+}
